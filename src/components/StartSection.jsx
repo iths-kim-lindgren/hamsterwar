12859d694c4447d4a229d3f950996b8c86edb260
Kim Lindgren <kim.johan.lindgren@gmail.com>
2021-02-15 22:16:08 +0100

    changed startpage

--- src/components/StartSection.jsx
+++ src/components/StartSection.jsx
@@ -9,14 +9,21 @@ const StyledArticle = styled.article`
     flex-direction: row;
     align-items: flex-end;
     justify-content: space-around;
+    & article {
+        height: 100%;
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+    }
     & img {
-        height: 30vh;
+        opacity: 0.7;
     }
     & p {
         width: 50vw;
-        height: 35vh;
-        font-family: 'Righteous', 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
         color: black;
+        text-align: center;
+        font-size: 1.2rem;
     }
 `
 
@@ -25,9 +32,12 @@ const StartSection = () => {
     return (
         <MainSection>
             <StyledArticle>
-                <p>The evil otter lord Aurelius is capturing hamsters from all over the world and trapping them in the dungeon below his gladiator arena. Every night, several hamsters are selected to fight each other to the death - although the defeated hamster may live, if Aurelius is merciful.
-Lord Aurelius will be greatly pleased if you submit a hamster of your own to his killing fields... Or you can simply join him at the balcony and watch a single battle. </p>
-                <img src="./hamsterIMG/evilOtter.png" alt="evilOtter" className="evil-otter"></img>
+                <img src="./hamsterIMG/torch.webp" alt="torch" className="evil-otter"></img>
+                <article>
+                    <p>The evil otter lord Aurelius is capturing hamsters from all over the world and trapping them in the dungeon below his gladiator arena. Every night, several hamsters are selected to fight each other to the death - although the defeated hamster may live, if Aurelius is merciful.</p>
+                    <p>Lord Aurelius will be greatly pleased if you submit a hamster of your own to his killing fields... Or you can simply join him at the balcony and watch a single battle. </p>
+                </article>
+                <img src="./hamsterIMG/torch.webp" alt="torch" />
             </StyledArticle>
         </MainSection>
     )
